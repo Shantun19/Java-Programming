7896396632f7130f58c2fd201ee3d749977fe02a
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2026-02-01 22:06:13 +0530

    Create 01_IntroductionToJava.java

---
+++ 01_IntroductionToJava.java
@@ -0,0 +1,14 @@
+/* Introdunction to Java 
+    - java is a high level object oriented programming language used to build the web apps , mobile apps and enterprise software systems.
+    - java is a platform independent language which means the code written in java can run on any device that support the jvm,
+*/
+
+/*
+    basic code example of java 
+    
+    public class HelloWorld {
+        public static void main(String[] args) {
+            System.out.println("Hello World");
+        }
+    }
+*/
