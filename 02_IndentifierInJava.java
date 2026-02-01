ad3f75226625e165f54938e6477531cc7639c5a1
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2026-02-01 22:12:47 +0530

    Create 02_IndentifierInJava.java

---
+++ 02_IndentifierInJava.java
@@ -0,0 +1,27 @@
+/*
+    Identifiers in Java
+        - a Name in java is called an identifiers which can be used for identification purpose it can be a method name , class name or a varaible name.
+example 
+
+Q.how many identifier in the below code sample ? 
+
+*/
+
+
+class Test {
+    public static void main(String[] args) {
+        int x = 10;
+    }
+}
+
+/*
+
+Ans :
+
+1. Test - Name of the class
+2. main - method name 
+3. x - name of the varaible
+4. args - name of the argument (considered as variable)
+5. String - the name of the String class in java
+
+*/
