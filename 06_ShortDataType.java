33b3c1a05acfdc51a74502d18af39e184eaf73ba
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2026-02-14 18:49:24 +0530

    Create 06_ShortDataType.java

---
+++ 06_ShortDataType.java
@@ -0,0 +1,19 @@
+/*
+
+short Data type 
+    - size = 2 byte = 16bits
+    - Min value = -23768
+    - Max value = 23767
+    - Range = -23768 to 23767
+*/
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        short max = 127;
+        short min = -128;
+        
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+    }
+}
