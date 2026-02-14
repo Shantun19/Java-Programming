fd97664667a59df848036efd990b9febff28d3f2
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2026-02-14 18:51:23 +0530

    Create 07_intDataType.java

---
+++ 07_intDataType.java
@@ -0,0 +1,17 @@
+/*
+
+int Data type 
+    - size = 4 byte = 32 bits
+    - Min value = -2^31
+    - Max value = 2^31 - 1
+    - Range = -2147483648 to 2147483647
+*/
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+}
